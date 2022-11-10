b7e87133761c7989be6364373602ddd18b676a3f
Camelia Braghes <cameliabraghes@gmail.com>
2022-11-10 11:03:06 +0200

    Remove navigation view

--- Scrumdinger/Views/DetailView.swift
+++ Scrumdinger/Views/DetailView.swift
@@ -7,66 +7,64 @@ struct DetailView: View {
     @Binding var scrum: DailyScrum
     
     var body: some View {
-        NavigationView {
-            List {
-                Section(header: Text("Meeting Info")) {
-                    NavigationLink(destination: MeetingView()) {
-                        Label("Start Meeting", systemImage: "timer")
-                            .foregroundColor(.blue)
-                            .font(.headline)
-                    }
-                    
-                    HStack {
-                        Label("Lengh", systemImage:  "clock")
-                        Spacer()
-                        Text("\(scrum.lengthInMinutes) minute")
-                    }
-                    .accessibilityElement(children: .combine)
-                    
-                    HStack {
-                        Label("Theme", systemImage: "paintpalette")
-                        Spacer()
-                        Text(scrum.theme.name)
-                            .padding(4)
-                            .background(scrum.theme.mainColor)
-                            .cornerRadius(4)
-                            .shadow(radius: 4)
-                    }
+        List {
+            Section(header: Text("Meeting Info")) {
+                NavigationLink(destination: MeetingView()) {
+                    Label("Start Meeting", systemImage: "timer")
+                        .foregroundColor(.blue)
+                        .font(.headline)
                 }
                 
-                Section(header: Text("Attendees")) {
-                    ForEach(scrum.attendees) { attendee in
-                        Label("\(attendee.name)", systemImage: "person")
-                    }
+                HStack {
+                    Label("Lengh", systemImage:  "clock")
+                    Spacer()
+                    Text("\(scrum.lengthInMinutes) minute")
+                }
+                .accessibilityElement(children: .combine)
+                
+                HStack {
+                    Label("Theme", systemImage: "paintpalette")
+                    Spacer()
+                    Text(scrum.theme.name)
+                        .padding(4)
+                        .background(scrum.theme.mainColor)
+                        .cornerRadius(4)
+                        .shadow(radius: 4)
                 }
             }
-            .navigationTitle(scrum.title)
-            .toolbar {
-                Button {
-                    isPresentingEditView = true
-                    data = scrum.data
-                } label: {
-                    Text("Edit")
+            
+            Section(header: Text("Attendees")) {
+                ForEach(scrum.attendees) { attendee in
+                    Label("\(attendee.name)", systemImage: "person")
                 }
             }
-            .sheet(isPresented: $isPresentingEditView) {
-                NavigationView {
-                    DetailEditView(data: $data)
-                        .navigationTitle(scrum.title)
-                        .toolbar {
-                            ToolbarItem(placement: .cancellationAction) {
-                                Button("Cancel") {
-                                    isPresentingEditView = false
-                                }
-                            }
-                            ToolbarItem(placement: .confirmationAction) {
-                                Button("Done") {
-                                    isPresentingEditView = false
-                                    scrum.update(from: data)
-                                }
+        }
+        .navigationTitle(scrum.title)
+        .toolbar {
+            Button {
+                isPresentingEditView = true
+                data = scrum.data
+            } label: {
+                Text("Edit")
+            }
+        }
+        .sheet(isPresented: $isPresentingEditView) {
+            NavigationView {
+                DetailEditView(data: $data)
+                    .navigationTitle(scrum.title)
+                    .toolbar {
+                        ToolbarItem(placement: .cancellationAction) {
+                            Button("Cancel") {
+                                isPresentingEditView = false
                             }
                         }
-                }
+                        ToolbarItem(placement: .confirmationAction) {
+                            Button("Done") {
+                                isPresentingEditView = false
+                                scrum.update(from: data)
+                            }
+                        }
+                    }
             }
         }
     }
