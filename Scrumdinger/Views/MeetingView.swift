4ee016d2f88321b873ee32b78f1c22659a416d00
Camelia Braghes <cameliabraghes@gmail.com>
2022-11-10 11:41:49 +0200

    Create an overlay view

--- Scrumdinger/Views/MeetingView.swift
+++ Scrumdinger/Views/MeetingView.swift
@@ -1,47 +1,58 @@
 import SwiftUI
 
 struct MeetingView: View {
+    @Binding var scrum: DailyScrum
+    
     var body: some View {
-        VStack {
-            ProgressView(value: 5, total: 15)
+        ZStack {
+            RoundedRectangle(cornerRadius: 4.0)
+                .fill(scrum.theme.mainColor)
             
-            HStack {
-                VStack {
-                    Text("Seconds Elapsed")
-                    Label("300", systemImage: "hourglass.bottomhalf.fill")
-                }
-                .font(.caption)
-            
-                Spacer()
+            VStack {
+                ProgressView(value: 5, total: 15)
                 
-                VStack {
-                    Text("Seconds Remaining")
-                    Label("600", systemImage: "hourglass.tophalf.fill")
+                HStack {
+                    VStack {
+                        Text("Seconds Elapsed")
+                        Label("300", systemImage: "hourglass.bottomhalf.fill")
+                    }
+                    .font(.caption)
+                
+                    Spacer()
+                    
+                    VStack {
+                        Text("Seconds Remaining")
+                        Label("600", systemImage: "hourglass.tophalf.fill")
+                    }
+                    .font(.caption)
                 }
-                .font(.caption)
-            }
-            .accessibilityElement(children: .ignore)
-            .accessibilityLabel("Time remaining")
-            .accessibilityValue("10 minutes")
-            
-            Circle()
-                .strokeBorder(lineWidth: 24)
-            
-            HStack {
-                Text("Speaker 1 of 3")
-                Spacer()
-                Button(action: {}) {
-                    Image(systemName: "forward.fill")
+                .accessibilityElement(children: .ignore)
+                .accessibilityLabel("Time remaining")
+                .accessibilityValue("10 minutes")
+                
+                Circle()
+                    .strokeBorder(lineWidth: 24)
+                
+                HStack {
+                    Text("Speaker 1 of 3")
+                    Spacer()
+                    Button(action: {}) {
+                        Image(systemName: "forward.fill")
+                    }
+                    .accessibilityLabel("Next speaker")
                 }
-                .accessibilityLabel("Next speaker")
             }
+           
+            
         }
         .padding()
+        .foregroundColor(scrum.theme.accentColor)
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
 struct MeetingView_Previews: PreviewProvider {
     static var previews: some View {
-        MeetingView()
+        MeetingView(scrum: .constant(DailyScrum.sampleData[0]))
     }
 }
